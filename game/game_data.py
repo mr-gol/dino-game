ec8cb447610d5683dac3865fa2ca2e45092b5537
mr-gol <mgolubtsovam@gmail.com>
2022-04-24 19:48:08 +0300

    .

---
+++ game/game_data.py
@@ -0,0 +1,29 @@
+level_0 = {'ground': 'levels/0/level_0_ground.csv',
+		'flower': 'levels/0/level_0_flower.csv',
+		'player': 'levels/0/level_0_player.csv',
+		#'details': 'levels/0/level_0_detail.csv',
+		#'human': 'levels/0/level_0_human.csv',
+		'unlock': 2}
+
+level_1 = {'ground': 'levels/0/level_0_ground.csv',
+		'flower': 'levels/0/level_0_flower.csv',
+		'player': 'levels/0/level_0_player.csv', #,
+		#'details': 'levels/0/level_0_detail.csv',
+		#'human': 'levels/0/level_0_human.csv'
+		'unlock': 2}
+
+level_2 = {'ground': 'levels/0/level_0_ground.csv',
+		'flower': 'levels/0/level_0_flower.csv',
+		'player': 'levels/0/level_0_player.csv', #,
+		#'details': 'levels/0/level_0_detail.csv',
+		#'human': 'levels/0/level_0_human.csv'
+		'unlock': 3}
+
+level_3 = {'ground': 'levels/0/level_0_ground.csv',
+		'flower': 'levels/0/level_0_flower.csv',
+		'player': 'levels/0/level_0_player.csv',
+		#'details': 'levels/0/level_0_detail.csv',
+		#'human': 'levels/0/level_0_human.csv'
+		'unlock': 3}
+
+levels = {0: level_0, 1: level_1, 2: level_2, 3: level_3}
