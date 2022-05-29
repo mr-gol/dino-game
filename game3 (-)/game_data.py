e0751ac151b2022bbe44b0662072ccdb3e82b0ed
mr-gol <mgolubtsovam@gmail.com>
2022-05-29 11:17:37 +0300

    .

--- game3 (-)/game_data.py
+++
@@ -1,7 +0,0 @@
-level_0 = {'ground': 'levels/0/level_0_ground.csv',
-		'flower': 'levels/0/level_0_flower.csv',
-		'player': 'levels/0/level_0_player.csv',
-		'trash': 'levels/0/level_0_trash.csv'}
-		#'human': 'levels/0/level_0_human.csv'
-
-levels = {0: level_0}
